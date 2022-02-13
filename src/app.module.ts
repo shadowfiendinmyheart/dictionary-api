1752bf22ada8525dff00282097ab376a9918720d
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2022-02-13 17:31:09 +0300

    connect to database & init users folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    controllers: [],
+    providers: [ ],
+    imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env'
+        }),
+        SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
+          models: [],
+          autoLoadModels: true,
+          dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+        }),
+        UsersModule
+      ],
 })
 export class AppModule {}
