440c57598293ccab6af0bd2ac5e35294510f2ea5
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2022-04-23 20:53:01 +0300

    [add] prepare for deploy on heroku

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Dictionary app')
